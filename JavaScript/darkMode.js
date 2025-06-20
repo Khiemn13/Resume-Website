d16c2c23014c0234be26267f2b09ccaac3519558
khiemn <khiem.nguyen@utas.edu.au>
2025-06-20 16:05:08 +1000

    added project card 1 turnbased game and some holder content. Added Darkmode so it saved locally

--- JavaScript/darkMode.js
+++ JavaScript/darkMode.js
@@ -1,15 +1,41 @@
 function toggleDarkMode() {
   const body = document.body;
-  body.classList.toggle('dark-mode');
-
   const navbar = document.querySelector('.navbar');
-  
+  const cards = document.querySelectorAll('.project-card');
+
+  const isDark = body.classList.toggle('dark-mode');
+
   if (navbar) {
     navbar.classList.toggle('dark-mode');
   }
-  const cards = document.querySelectorAll('.project-card');
 
   cards.forEach(function(card) {
     card.classList.toggle('dark-mode');
   });
+
+  if (isDark) 
+    { localStorage.setItem('theme', 'dark');}
+  else 
+    { localStorage.setItem('theme', 'light');}
 }
+
+
+ // Save the theme preference in localStorage
+  window.addEventListener('DOMContentLoaded', () => {
+  const body = document.body;
+  const navbar = document.querySelector('.navbar');
+  const cards = document.querySelectorAll('.project-card');
+  const savedTheme = localStorage.getItem('theme');
+
+
+
+  if (savedTheme === 'dark') {
+    body.classList.toggle('dark-mode');
+    navbar.classList.toggle('dark-mode');
+    
+    cards.forEach(function(card) {
+      card.classList.toggle('dark-mode');
+  });
+  
+  }
+  });
